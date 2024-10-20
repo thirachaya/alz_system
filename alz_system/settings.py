8f038c1fefceb7df7fb59d956732ce69cc2f66be
Teerachaya <teerachaya@192.168.1.103>
2024-10-21 02:42:09 +0700

    Update settings for Render deployment

--- alz_system/settings.py
+++ alz_system/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-plwzna%^aksxoj1a10p(30@hlz8kj#aoman_d__=!4l&doi+kx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['alzheimerdiagnosis.onrender.com', 'localhost']
 
